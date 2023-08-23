af4b693ce4a32af0cc6dedc4447a2c167b51ab4a
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2023-08-23 14:25:52 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
-const express = require('express');
-
-const app = express();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -44,54 +41,3 @@ for (const file of eventFiles) {
 }
 
 client.login(process.env.TOKEN);
-
-app.get('/register_commands', async (req, res) => {
-	const commands = [];
-
-	const foldersPathReg = path.join(__dirname, 'commands');
-	const commandFoldersReg = fs.readdirSync(foldersPathReg);
-
-	for (const folder of commandFoldersReg) {
-		const commandsPath = path.join(foldersPathReg, folder);
-		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-		for (const file of commandFiles) {
-			const filePath = path.join(commandsPath, file);
-			const command = require(filePath);
-
-			if ('data' in command && 'execute' in command) {
-				commands.push(command.data.toJSON());
-			} else {
-				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-			}
-		}
-	}
-
-	const rest = new REST().setToken(process.env.TOKEN);
-
-	(async () => {
-		try {
-			console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-			const data = await rest.put(
-				// Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-				Routes.applicationCommands(process.env.CLIENT_ID),
-				{ body: commands },
-			);
-
-			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-			res.send(`Successfully reloaded ${data.length} application (/) commands.`);
-		} catch (error) {
-			console.error(error);
-			res.send(error);
-		}
-	})();
-});
-
-app.get('/', async (req, res) => {
-	return res.send('Haiko desu');
-});
-
-app.listen(3000, () => {
-	console.log(`Express server is running on port ${3000}`);
-});
